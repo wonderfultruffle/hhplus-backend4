e406955424d7fade25520824ade80f6f24e3c656
jh <jh2two@gmail.com>
2024-03-20 00:24:30 +0900

    Test: Point 충전 Test 메서드 추가
    - PoingControllerTest.java: 포인트_최소_금액_미만_충전_실패(), 포인트_최대_금액_초과_충전_실패() 추가

--- src/test/java/io/hhplus/tdd/point/PointControllerTest.java
+++ src/test/java/io/hhplus/tdd/point/PointControllerTest.java
@@ -1,8 +1,6 @@
 package io.hhplus.tdd.point;
 
-import io.hhplus.tdd.database.UserPointTable;
 import org.junit.jupiter.api.Test;
-
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -10,53 +8,50 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 class PointControllerTest {
     private PointController controller = new PointController();
+
+    UserPoint create_user_point(Long id) throws Exception{
+        // UserPoint 객체를 생성
+        return controller.point(id);
+    }
     @Test
     void 사용자를_생성하여_포인트_조회() throws Exception {
         // point 성공 케이스 1
         // 지정한 ID 를 갖는 사용자의 Point Data 가 조회되어야 한다.
         Long userId = 1L;
-        assertEquals(controller.point(userId).id(), userId);
+        assertEquals(userId, create_user_point(userId).id());
     }
     @Test
     void 이미_존재하는_사용자의_포인트_조회() throws Exception {
         // point 성공 케이스 2
-        // 이미 존재하는 사용자 id 에 대해선 UserPoint 가 새로 생성되지 않고
-        // UserPointTable 에 저장된 Data 가 조회되어야 한다.
+        // 이미 존재하는 사용자 id 에 대해선 UserPoint 가 새로 생성되지 않고 UserPointTable 에 저장된 Data 가 조회되어야 한다.
         Long userId = 1L;
-        UserPoint point = controller.point(userId);
+        UserPoint point = create_user_point(userId);
 //        assertEquals(point.id(), userId); // 사용자를_생성하여_포인트_조회() 의 test 를 포함할 수 있는 내용이 중복되어도 괜찮은가?
 
         Long saveTime = point.updateMillis();
-        assertEquals(controller.point(userId).updateMillis(), saveTime);
+        assertEquals(saveTime, controller.point(userId).updateMillis());
     }
-//    @Test
-//    void 포인트_충전_성공() throws InterruptedException {
-//        // charge 성공 케이스
-//        Long user_id = 1L;
-//        Long init_pt = 0L;
-//        UserPointTable pt_tbl = new UserPointTable();
-//        pt_tbl.insertOrUpdate(user_id, init_pt);
-//
-//        PointController pt_ctrl = new PointController();
-//        Long amount = 1000L;
-//        assertEquals(pt_ctrl.charge(user_id, amount), init_pt + amount);
-//    }
-//
-//    @Test
-//    void 포인트_최소_충전_금액_미달() throws InterruptedException {
-//        // charge 실패 케이스 2: 충전 가능 최소 금액 미만
-//        Long user_id = 1L;
-//        Long init_pt = 0L;
-//        UserPointTable pt_tbl = new UserPointTable();
-//        pt_tbl.insertOrUpdate(user_id, init_pt);
-//
-//        PointController pt_ctrl = new PointController();
-//        Long amount = 1L;
-//        // 실패 확인 메서드
-//        // 포인트 충전은 실패하고 잔액은 충전 전과 동일해야 한다.
-//        assertEquals(pt_ctrl.charge(user_id, amount), init_pt);
-//    }
-//
+
+    @Test
+    void 포인트_최소_금액_미만_충전_실패() throws Exception {
+        Long userId = 1L;
+        Long userPoint = create_user_point(userId).point();
+
+        Long amount = 999L;  // 충전 가능한 최소 금액은 1000L 로 설정한다.
+        // 충전 금액이 이전과 동일해야 함.
+        assertEquals(userPoint, controller.charge(userId, amount).point());
+    }
+    
+    @Test
+    void 포인트_최대_금액_초과_충전_실패() throws Exception {
+        Long userId = 1L;
+        Long userPoint = create_user_point(userId).point();
+
+        Long amount = 100000000L; // 충전 가능한 금액은 최대 99999999L 로 설정한다.
+        // 충전 금액이 이전과 동일해야 함.
+        assertEquals(userPoint, controller.charge(userId, amount).point());
+    }
+
 //    @Test
 //    void 포인트_사용_성공() throws InterruptedException {
 //        // use 성공 케이스
